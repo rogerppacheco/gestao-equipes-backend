6ac3c8e015ac011d1b84c620831cc7ad887bdc94
rogerppacheco <roggerio@gmail.com>
2025-06-28 23:49:03 -0300

    Feat: Adiciona lógica de DB para produção e desenvolvimento

--- Config/db.js
+++ Config/db.js
@@ -1,14 +1,24 @@
+// Config/db.js
 require('dotenv').config();
 const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
+let pool;
+
+// Verifica se está no ambiente de produção do Heroku (procurando pela JAWSDB_URL)
+if (process.env.JAWSDB_URL) {
+    // Se estiver no Heroku, usa a URL de conexão completa
+    pool = mysql.createPool(process.env.JAWSDB_URL);
+} else {
+    // Se estiver na sua máquina local, usa as variáveis do arquivo .env
+    pool = mysql.createPool({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0
+    });
+}
 
 module.exports = pool;
